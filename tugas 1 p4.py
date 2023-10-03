12a71cfc747280ec40cb955032ab3d7e08180604
ahmadaji32 <101126549+ahmadaji32@users.noreply.github.com>
2023-10-03 21:18:10 +0700

    tugas 1 p4.py

--- tugas 1 p4.py
+++ tugas 1 p4.py
@@ -1,58 +1,58 @@
-# Dictionary untuk menentukan gaji pokok berdasarkan golongan jabatan
-gaji_pokok_per_golongan = {
-    "Golongan 1": 5000000,
-    "Golongan 2": 6000000,
-    "Golongan 3": 7000000,
-    "Golongan 4": 8000000
+# Gaji pokok untuk karyawan kontrak
+gaji_pokok_karyawan_kontrak = 300000
+
+# Dictionary untuk menentukan persentase tunjangan jabatan berdasarkan golongan
+persentase_tunjangan_jabatan = {
+    1: 0.05,
+    2: 0.10,
+    3: 0.15
 }
 
-# Dictionary untuk menentukan tunjangan jabatan
-tunjangan_jabatan = {
-    "Golongan 1": 1000000,
-    "Golongan 2": 1500000,
-    "Golongan 3": 2000000,
-    "Golongan 4": 2500000
-}
-
-# Dictionary untuk menentukan tunjangan pendidikan
-tunjangan_pendidikan = {
-    "SMA": 500000,
-    "Diploma": 1000000,
-    "Sarjana": 1500000,
-    "Magister": 2000000,
-    "Doktor": 2500000
+# Dictionary untuk menentukan persentase tunjangan pendidikan berdasarkan pendidikan
+persentase_tunjangan_pendidikan = {
+    "SMA": 0.025,
+    "D1": 0.05,
+    "D3": 0.20,
+    "S1": 0.30
 }
 
 # Tarif lembur per jam
-tarif_lembur_per_jam = 50000
+tarif_lembur_per_jam = 3500
 
 # Input nama karyawan, golongan jabatan, pendidikan, dan jumlah jam kerja
 nama_karyawan = input("Nama karyawan: ")
-golongan_jabatan = input("Golongan jabatan (Golongan 1/Golongan 2/Golongan 3/Golongan 4): ")
-pendidikan = input("Pendidikan (SMA/Diploma/Sarjana/Magister/Doktor): ")
+golongan_jabatan = int(input("Golongan jabatan (1/2/3): "))
+pendidikan = input("Pendidikan (SMA/D1/D3/S1): ")
 jam_kerja = float(input("Jumlah jam kerja: "))
 
-# Memeriksa apakah golongan jabatan dan pendidikan valid
-if golongan_jabatan in gaji_pokok_per_golongan and pendidikan in tunjangan_pendidikan:
-    gaji_pokok = gaji_pokok_per_golongan[golongan_jabatan]
-    tunjangan_jabatan = tunjangan_jabatan[golongan_jabatan]
-    tunjangan_pendidikan = tunjangan_pendidikan[pendidikan]
-    
-    # Menghitung honor lembur
-    if jam_kerja > 40:
-        lembur = (jam_kerja - 40) * tarif_lembur_per_jam
-    else:
-        lembur = 0
-
-    # Menghitung total gaji
-    total_gaji = gaji_pokok + tunjangan_jabatan + tunjangan_pendidikan + lembur
-
-    # Menampilkan hasil
-    print(f"Karyawan yang bernama {nama_karyawan}")
-    print(f"Honor yang diterima (Gaji Pokok): Rp {gaji_pokok:,.0f}")
-    print(f"Tunjangan Jabatan: Rp {tunjangan_jabatan:,.0f}")
-    print(f"Tunjangan Pendidikan: Rp {tunjangan_pendidikan:,.0f}")
-    print(f"Honor Lembur: Rp {lembur:,.0f}")
-    print(f"Total Gaji: Rp {total_gaji:,.0f}")
+# Menghitung tunjangan jabatan
+if golongan_jabatan in persentase_tunjangan_jabatan:
+    persentase_jabatan = persentase_tunjangan_jabatan[golongan_jabatan]
+    tunjangan_jabatan = gaji_pokok_karyawan_kontrak * persentase_jabatan
 else:
-    print("Golongan jabatan atau pendidikan tidak valid.")
+    tunjangan_jabatan = 0
+
+# Menghitung tunjangan pendidikan
+if pendidikan in persentase_tunjangan_pendidikan:
+    persentase_pendidikan = persentase_tunjangan_pendidikan[pendidikan]
+    tunjangan_pendidikan = gaji_pokok_karyawan_kontrak * persentase_pendidikan
+else:
+    tunjangan_pendidikan = 0
+
+# Menghitung honor lembur
+if jam_kerja > 8:
+    jam_lembur = jam_kerja - 8
+    honor_lembur = jam_lembur * tarif_lembur_per_jam
+else:
+    honor_lembur = 0
+
+# Menghitung total gaji
+total_gaji = gaji_pokok_karyawan_kontrak + tunjangan_jabatan + tunjangan_pendidikan + honor_lembur
+
+# Menampilkan hasil
+print(f"Karyawan yang bernama {nama_karyawan}")
+print(f"Honor yang diterima (Gaji Pokok): Rp {gaji_pokok_karyawan_kontrak:,.0f}")
+print(f"Tunjangan Jabatan: Rp {tunjangan_jabatan:,.0f}")
+print(f"Tunjangan Pendidikan: Rp {tunjangan_pendidikan:,.0f}")
+print(f"Honor Lembur: Rp {honor_lembur:,.0f}")
+print(f"Total Gaji: Rp {total_gaji:,.0f}")
